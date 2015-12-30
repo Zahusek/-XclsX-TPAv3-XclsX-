31c4321a0b2a54afd2058792b2a33e80bf14aa79
ZahuseK <zahusek@gmail.com>
2015-12-30 23:02:21 +0100

    Create TabAPI.java

---
+++ com/gmial/zahusek/tinyprotocolapi/tabapi/TabAPI.java
@@ -0,0 +1,59 @@
+package com.gmail.zahusek.tinyprotocolapi.tabapi;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+
+import com.gmail.zahusek.tinyprotocolapi.TinyProtocolAPI;
+import com.google.common.collect.Maps;
+
+public class TabAPI implements Listener {
+	
+	
+	private static final Map<UUID, TabData> CELLS = Maps.newHashMap();
+	
+	private static TabData getPlayer(Player player) {
+		if(!CELLS.containsKey(player.getUniqueId())) {
+			CELLS.put(player.getUniqueId(), new TabData());
+			base(player);
+		}
+		return CELLS.get(player.getUniqueId());
+	}
+	public static void setSlot(Player player, int x, int y, String msg, int ping) {
+		getPlayer(player).setCell(x, y, msg, ping);
+	}
+	public static void setSkin(Player player, int x, int y, String skin) {
+		getPlayer(player).setSkin(x, y, skin);
+	}
+	public static void setHnF(Player player, String header, String footer) {
+		getPlayer(player).setHnF(header, footer);
+	}
+	public static void refresh(Player player) {
+		getPlayer(player).getRefresh().forEach((packet) -> packet.sendPacket(player));
+	}
+	public static void base(Player player) {
+		getPlayer(player).getDefault().forEach((packet) -> packet.sendPacket(player));
+	}
+	protected void remove(Player player) {
+		getPlayer(player).getRemove().forEach((packet) -> packet.sendPacket(player));
+		CELLS.remove(player.getUniqueId());
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void quit(PlayerQuitEvent e) {
+		remove(e.getPlayer());
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void disable(PluginDisableEvent e) {
+		if(e.getPlugin() != TinyProtocolAPI.getPlugin(TinyProtocolAPI.class)) return;
+		Bukkit.getOnlinePlayers().forEach((player) -> remove(player));
+	}
+}
