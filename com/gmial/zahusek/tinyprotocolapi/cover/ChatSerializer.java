093d660b8c4336c706c2c6b8bca50a9828094e5c
ZahuseK <zahusek@gmail.com>
2016-01-02 16:43:33 +0100

    Update ChatSerializer.java

--- com/gmial/zahusek/tinyprotocolapi/cover/ChatSerializer.java
+++ com/gmial/zahusek/tinyprotocolapi/cover/ChatSerializer.java
@@ -1,24 +1,27 @@
-package com.gmail.zahusek.tinyprotocolapi.reflect;
+package com.gmail.zahusek.tinyprotocolapi.cover;
 
 import org.apache.commons.lang.StringUtils;
 
 import com.gmail.zahusek.tinyprotocol.Reflection;
+import com.gmail.zahusek.tinyprotocol.Reflection.MethodInvoker;
 
 public class ChatSerializer {
+	
+	private static final MethodInvoker ichat = Reflection.getTypedMethod(
+			Reflection.getClass("{nms}.IChatBaseComponent$ChatSerializer"),
+			"a", Reflection.getClass("{nms}.IChatBaseComponent"),
+			String.class);
+	private static final MethodInvoker chat = Reflection.getTypedMethod(
+			Reflection.getClass("{nms}.IChatBaseComponent$ChatSerializer"),
+			"a", String.class,
+			Reflection.getClass("{nms}.IChatBaseComponent"));
 
 	public static Object toIChat(String msg) {
-		return Reflection.getTypedMethod(
-				Reflection.getClass("{nms}.IChatBaseComponent$ChatSerializer"),
-				"a", Reflection.getClass("{nms}.IChatBaseComponent"),
-				String.class).invoke(null, String.format("{text:'%s'}", msg));
+		return ichat.invoke(null, String.format("{text:'%s'}", msg));
 	}
 
 	public static String toChat(Object obs) {
-		String plain = (String) Reflection.getTypedMethod(
-				Reflection.getClass("{nms}.IChatBaseComponent$ChatSerializer"),
-				"a", String.class,
-				Reflection.getClass("{nms}.IChatBaseComponent")).invoke(null,
-				obs);
-		return StringUtils.strip(plain, "\"");
+		return StringUtils.strip(((String)chat.invoke(null, obs)), "\"");
 	}
 }
+
